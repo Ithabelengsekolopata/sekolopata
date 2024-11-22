eba1348cdfd5ea5722ed969aefcf7fa71e9ffc86
Your Name <you@example.com>
2024-11-22 18:04:12 +0200

    Initial commit

---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+
+
+const Home = () => {
+    return (
+        <div className="home">
+            <h1> Best System Ever</h1>
+           
+            <img src="ww.webp" class="rotating-image" alt="Rotating Image"
+           />
+           <h1>ENJOY TO THE FULLEST</h1>
+            
+        </div>
+    );
+};
+
+export default Home;
